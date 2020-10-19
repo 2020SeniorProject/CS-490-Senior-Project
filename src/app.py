742c375458b87ad626327c9133e6ec636bbcb814
greeha01 <greeha01.luther.edu>
2020-10-18 21:28:20 -0500

    Simplified Template, Added initiative sockets

--- src/app.py
+++ src/app.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template
+from threading import Lock
+from flask import Flask, render_template, session
+from flask_socketio import SocketIO, emit 
 
+async_mode = None
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, async_mode=async_mode)
+thread = None
+thread_lock = Lock()
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", async_mode=socketio.async_mode)
+
+@socketio.on('my_broadcast_event', namespace='/test')
+def test_broadcast_message(message):
+    # session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response', {'data': message['data']}, broadcast=True)
+    # emit('my_response', {'data': message['data'], 'count': session['receive_count']}, broadcast=True)
--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,23 +1,54 @@
-function openNav(id) {
-    document.getElementById("initiative").style.width = "16.66666667%";
-  }
+// function addPlayer() {
+//   let playerName = document.querySelector("#player_name").value;
+//   let initiative = document.querySelector("#initiative_roll").value;
+
+//   console.log(playerName, initiative);
+// }
+
+$(document).ready(function() {
+  // Use a "/test" namespace.
+  // An application can open a connection on multiple namespaces, and
+  // Socket.IO will multiplex all those connections on a single
+  // physical channel. If you don't care about multiple channels, you
+  // can set the namespace to an empty string.
+  namespace = '/test';
   
-  function closeNav(id) {
-    document.getElementById("initiative").style.width = "0";
-  } 
+  // Connect to the Socket.IO server.
+  // The connection URL has the following format, relative to the current page:
+  //     http[s]://<domain>:<port>[/<namespace>]
+  var socket = io(namespace);
 
-function openTools(id) {
-    document.getElementById("dm-tools").style.width = "16.66666667%";
-}
+  $('form#set_initiative').submit(function(event) {
+    socket.emit('my_broadcast_event', {data: [$('#player_name').val(), $('#initiative_roll').val()]});
+    console.log([$('#player_name').val(), $('#initiative_roll').val()])
+    return false;
+  });
 
-function closeTools(id) {
-    document.getElementById("dm-tools").style.width = "0";
-  } 
+  socket.on('my_response', function(msg) {
+    $('#initiative-list').append('<br>' + $('<div/>').text('Received ' + msg.data).html());
+  });
+});
 
-function openLogs(id) {
-    document.getElementById("log").style.height = "16.66666667%";
-}
+// function openNav(id) {
+//     document.getElementById("initiative").style.width = "16.66666667%";
+//   }
+  
+//   function closeNav(id) {
+//     document.getElementById("initiative").style.width = "0";
+//   } 
 
-function closeLogs(id) {
-    document.getElementById("log").style.height = "0";
-}
+// function openTools(id) {
+//     document.getElementById("dm-tools").style.width = "16.66666667%";
+// }
+
+// function closeTools(id) {
+//     document.getElementById("dm-tools").style.width = "0";
+//   } 
+
+// function openLogs(id) {
+//     document.getElementById("log").style.height = "16.66666667%";
+// }
+
+// function closeLogs(id) {
+//     document.getElementById("log").style.height = "0";
+// }
