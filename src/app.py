0f76ed13a42eddafc7eacac6fb453069070c7b7b
greeha01 <greeha01.luther.edu>
2020-11-02 11:39:05 -0600

    Refactor socketio functions

--- src/app.py
+++ src/app.py
@@ -36,35 +36,36 @@ def play():
 def test_broadcast_message(message):
     # Sends to all connected
     emit('initiative_update', {'data': message['data']}, broadcast=True)
+    emit('log_update', {'data': "Initiative Added"}, broadcast=True)
+
     add_to_db("init", (session_id, user_key, message['data'][0], message['data'][1]))
     s = datetime.datetime.now().isoformat(sep=' ',timespec='seconds')
     init_name = message['data'][0] + message['data'][1]
-    add_to_db("log", (session_id, user_key, "Init", init_name, s) )  #redundant??
-    emit('log_update', {'data': "Initiative update"}, broadcast=True)
+    add_to_db("log", (session_id, user_key, "Init", init_name, s) )
 
 @socketio.on('send_chat', namespace='/test')
 def test_broadcast_message(message):
     # Sends to all connected
-    # emit('chat_update', {'data': message['data']}, broadcast=True)
     emit('chat_update', {'data': message['data']}, broadcast=True)
+    emit('log_update', {'data': "Chat update"}, broadcast=True)
+
     s = datetime.datetime.now().isoformat(sep=' ',timespec='seconds')
     add_to_db("log", (session_id, user_key, "Chat", message['data'][0], s))
-    emit('log_update', {'data': "Chat update"}, broadcast=True)
 
 @socketio.on('connect', namespace='/test')
 def test_connect():
-    global initiative
     # Sends upon a new connection
     emit('log_update', {'data': "Connected"}, broadcast=True)
+
     s = datetime.datetime.now().isoformat(sep=' ',timespec='seconds')
     add_to_db("log", (session_id, user_key, "Connection", "User connected", s))
+
     init_items = read_db("initiative", "player_name, init_val")
     chat_items = read_db("log", "log", "where title = 'Chat'")
-    # print(init_items)
-    # print(chat_items[0][0])
+
     if init_items != []:
         emit('initiative_update', {'data': init_items})
-        emit('log_update', {'data': "Initiative update"}, broadcast=True)
+        emit('log_update', {'data': "Initiative List Received"}, broadcast=True)
     if chat_items != []:
         emit('chat_update', {'data': chat_items})
-        emit('log_update', {'data': "Chat Update"}, broadcast=True)
+        emit('log_update', {'data': "Chat List Received"}, broadcast=True)
