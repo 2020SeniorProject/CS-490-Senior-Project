e47071783d5cabfd2a4fee58d9502c8b01184da7
greeha01 <greeha01.luther.edu>
2020-11-01 21:05:16 -0600

    Refactoring DB code

--- src/app.py
+++ src/app.py
@@ -17,9 +17,8 @@ thread_lock = Lock()
 session_id = "69BBEG69" #placeholder till we figure out id creation method, use usernames with random numbers?
 user_key = "BigGamer420" #placeholder till we create some sort of user database?
 
-test_conn = create_connection("battle_sesh.db")
-create_db_init(test_conn)
-create_db_log(test_conn)
+create_db_init()
+create_db_log()
 
 @app.route("/")
 def index():
@@ -31,47 +30,37 @@ def home():
     
 @app.route("/play")
 def play():
-    # conn = create_connection("battle_sesh.db")
-    # create_db_init(conn)
-    # create_db_log(conn)
-    #To Do -> need to create way to add information to the DBs via the UI 
-    # add_to_init(session_id, user_key, initiative)
     return render_template("index.html", async_mode=socketio.async_mode)
 
 
 @socketio.on('set_initiative', namespace='/test')
 def test_broadcast_message(message):
-    conn = create_connection("battle_sesh.db")
     # Sends to all connected
     emit('initiative_update', {'data': message['data']}, broadcast=True)
-    add_to_init(conn, session_id, user_key, message['data'][0], message['data'][1])
+    add_to_init(session_id, user_key, message['data'][0], message['data'][1])
     s = datetime.datetime.now().isoformat(sep=' ',timespec='seconds')
     init_name = message['data'][0] + message['data'][1]
-    add_to_log(conn, session_id, user_key, "Init", init_name, s)   #redundant??
+    add_to_log(session_id, user_key, "Init", init_name, s)   #redundant??
     emit('log_update', {'data': "Initiative update"}, broadcast=True)
-
 
 @socketio.on('send_chat', namespace='/test')
 def test_broadcast_message(message):
     # Sends to all connected
     # emit('chat_update', {'data': message['data']}, broadcast=True)
-    conn = create_connection("battle_sesh.db")
     emit('chat_update', {'data': message['data']}, broadcast=True)
     s = datetime.datetime.now().isoformat(sep=' ',timespec='seconds')
-    add_to_log(conn, session_id, user_key, "Chat", message['data'][0], s)
+    add_to_log(session_id, user_key, "Chat", message['data'][0], s)
     emit('log_update', {'data': "Chat update"}, broadcast=True)
-
 
 @socketio.on('connect', namespace='/test')
 def test_connect():
     global initiative
     # Sends upon a new connection
-    conn = create_connection("battle_sesh.db")
     emit('log_update', {'data': "Connected"}, broadcast=True)
     s = datetime.datetime.now().isoformat(sep=' ',timespec='seconds')
-    add_to_log(conn, session_id, user_key, "Connection", "User connected", s)
-    init_items = read_db_init(conn, "initiative", session_id)
-    chat_items = read_db_log_chat(conn, "log", session_id)
+    add_to_log(session_id, user_key, "Connection", "User connected", s)
+    init_items = read_db_init("initiative", session_id)
+    chat_items = read_db_log_chat("log", session_id)
     # print(init_items)
     # print(chat_items[0][0])
     if init_items != []:
--- src/log_init_db.py
+++ src/log_init_db.py
@@ -2,10 +2,6 @@ from flask import Flask, render_template, session, request
 from flask_socketio import SocketIO, emit 
 import sqlite3
 
-
-
-initiative = []
-
 # Log table
 # room_id = Global identifier for battle map created by user
 # user_key = Unique user ID, allows for maps to be sorted by users
@@ -13,26 +9,19 @@ initiative = []
 # Log = entry from log, entered by users. Tracked to actions in battle and other relevant info
 # timestamp = used to keep order of log entries for the room
 
-def create_db_log(conn):
-    cur = conn.cursor()              
-    cur.execute(f""" CREATE TABLE IF NOT EXISTS log (
-                                        room_id TEXT,
-                                        user_key TEXT,
-                                        title TEXT,     
-                                        log LONGTEXT,
-                                        timestamp DATETIME PRIMARY KEY
-                                    ); """)
+def create_db_log():
+    with create_connection("battle_sesh.db") as conn:
+        cur = conn.cursor()              
+        cur.execute(f"""CREATE TABLE IF NOT EXISTS log 
+                        (row_id INT PRIMARY KEY, room_id TEXT,user_key TEXT, title TEXT, log LONGTEXT, timestamp DATETIME); """)
 
 
-
-def add_to_log(conn, room_id, user_key, title, log, timestamp):
-    cur = conn.cursor()
-    sql = f"INSERT INTO log VALUES('{room_id}','{user_key}','{title}','{log}','{timestamp}');"
-    # item = (room_id, user_key, title, log, timestamp)
-    cur.execute(sql)
-    conn.commit()
-    # conn.close()
-
+def add_to_log(room_id, user_key, title, log, timestamp):
+    with create_connection("battle_sesh.db") as conn:
+        cur = conn.cursor()
+        sql = f"INSERT INTO log(room_id, user_key, title, log, timestamp) VALUES('{room_id}','{user_key}','{title}','{log}','{timestamp}');"
+        cur.execute(sql)
+        conn.commit()
 
 
 #initiative table
@@ -42,18 +31,18 @@ def add_to_log(conn, room_id, user_key, title, log, timestamp):
 #order = intiative # associated with a player
     # order allows for sorting
 
-def create_db_init(conn): 
-    cur = conn.cursor()
-    cur.execute(f"""CREATE TABLE IF NOT EXISTS initiative 
-                    (room_id TEXT PRIMARY KEY, user_key TEXT, player_name TEXT, init_val INT);""")    
+def create_db_init(): 
+    with create_connection("battle_sesh.db") as conn:
+        cur = conn.cursor()
+        cur.execute(f"""CREATE TABLE IF NOT EXISTS initiative 
+                        (row_id INT PRIMARY KEY, room_id TEXT, user_key TEXT, player_name TEXT, init_val INT);""")    
 
-def add_to_init(conn, room_id, user_key, player_name, init_val):
-    cur = conn.cursor()
-    sql = f"INSERT INTO initiative VALUES ('{room_id}', '{user_key}', '{player_name}', {init_val});"
-    # item = (room_id, user_key, player_name, order)
-    cur.execute(sql)
-    conn.commit()
-    # conn.close()
+def add_to_init(room_id, user_key, player_name, init_val):
+    with create_connection("battle_sesh.db") as conn:
+        cur = conn.cursor()
+        sql = f"INSERT INTO initiative(room_id, user_key, player_name, init_val) VALUES ('{room_id}', '{user_key}', '{player_name}', {init_val});"
+        cur.execute(sql)
+        conn.commit()
 
 
 def create_connection(db_file):
@@ -63,27 +52,23 @@ def create_connection(db_file):
 
 
 ## Selects player name and init # for reading
-def read_db_init(conn, db_name, room_id):       #streamline these
-    cur = conn.cursor() 
-    init = []
-    test = (room_id, )
-    # for row in cur.execute(f"select * from {db_name} where session_id = {room_id}"):
-    # for row in cur.execute(f"SELECT * FROM {db_name} where room_id = ?", test):
-    for row in cur.execute(f"SELECT player_name, init_val FROM {db_name}"):
-        init.append(row)
-    return init
+def read_db_init(db_name, room_id):       #streamline these
+    with create_connection("battle_sesh.db") as conn:
+        cur = conn.cursor() 
+        init = []
+        for row in cur.execute(f"SELECT player_name, init_val FROM {db_name}"):
+            init.append(row)
+        return init
 
 
 
 ## Read chats from log_db
-def read_db_log_chat(conn, db_name, room_id):       #streamline these
-    cur = conn.cursor() 
-    chat = []
-    test = (room_id, )
-    # for row in cur.execute(f"select * from {db_name} where session_id = {room_id}"):
-    # for row in cur.execute(f"SELECT * FROM {db_name} where room_id = ?", test):
-    for row in cur.execute(f"SELECT log FROM {db_name} where title = 'Chat'"):
-        chat.append(row)
-    return chat
+def read_db_log_chat(db_name, room_id):       #streamline these
+    with create_connection("battle_sesh.db") as conn:
+        cur = conn.cursor() 
+        chat = []
+        for row in cur.execute(f"SELECT log FROM {db_name} where title = 'Chat'"):
+            chat.append(row)
+        return chat
 
 
