b008430aae938e992a6ce8c410d37694d70cb9a8
treach01 <treach01@luther.edu>
2020-12-01 18:42:00 -0600

    Also fixed the confirm issue!

--- static/js/script.js
+++ static/js/script.js
@@ -39,11 +39,10 @@ $(document).ready(function() {
 
   $('form#close_room').submit(function(event) {
     // TODO: fix this 
-    // if (window.confirm("This will clear all initiative and chat data for this room and kick players. Map and character token locations will be saved. Proceed?") ){
-    //   socket.emit('end_room', {desc: "Close Room"});
-    //   return false;
-    // }
-    socket.emit('end_room', {desc:"Close Room"})
+    if (window.confirm("This will clear all initiative and chat data for this room and kick players. Map and character token locations will be saved. Proceed?") ){
+      socket.emit('end_room', {desc: "Close Room"});
+      return false;
+    }
     return false; 
   });
   
