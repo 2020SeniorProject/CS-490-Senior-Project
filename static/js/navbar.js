a22e94181f915d5f60eaf0ee43b82e19ad1d3279
greeha01 <greeha01.luther.edu>
2020-12-28 01:20:46 -0600

    Update navbar and increase width of items on homepage

---
+++ static/js/navbar.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    const route = $(location).attr('pathname');
+    const first_item = route.split('/')[1];
+
+    switch(first_item) {
+        case "home":
+        case "play":
+        case "characters":
+        case "user":
+            $(`#path_${first_item}`).addClass('active underline_text');
+            break;
+        default:
+            break;
+    }
+});
