155b951487fa66918e038d37e3096f4e88c84ee7
Daniel Opdahl <dopdahl16@gmail.com>
2021-04-11 16:10:12 -0500

    Added template for js event testing

--- tests/dm_play_script.test.js
+++ tests/dm_play_script.test.js
@@ -2,18 +2,18 @@
  * @jest-environment jsdom
  */
 
- const fs = require('fs');
- const path = require('path');
+const fs = require('fs');
+const path = require('path');
 
- jest.mock('../static/js/api_calls.js');
- jest.mock('../static/js/bootstrap.bundle.min.js');
- jest.mock('../static/js/map_image.js');
- jest.mock('../static/js/navbar.js');
- jest.mock('../static/js/page_resize.js');
- jest.mock('../static/js/script.js');
+jest.mock('../static/js/api_calls.js');
+jest.mock('../static/js/bootstrap.bundle.min.js');
+jest.mock('../static/js/map_image.js');
+jest.mock('../static/js/navbar.js');
+jest.mock('../static/js/page_resize.js');
+jest.mock('../static/js/script.js');
 
- jest
-    .dontMock('fs');
+jest
+  .dontMock('fs');
 
 // console.log(__dirname);
 const html = fs.readFileSync(path.resolve(__dirname, '../templates/play.html'), 'utf8');
@@ -32,17 +32,23 @@ let httpServerAddr;
 let ioServer;
 
 test('use jsdom in this test file', () => {
-    const $ = require('jquery');
-    window.$ = $;
-    const element = document.createElement('div');
-    expect(element).not.toBeNull();
-  });
+  const $ = require('jquery');
+  window.$ = $;
+  const element = document.createElement('div');
+  expect(element).not.toBeNull();
+});
 
 test('Test constants are as expected', () => {
-    const callback = jest.fn();
-    all(callback);
-    expect(namespace).toMatch('/combat');
-    expect(checklist).toMatch(`<ul id=checklist class="list-group list-group-flush">
+  const $ = require('jquery');
+  const callback = jest.fn();
+  all(callback);
+  expect(initiatives).toMatchObject([]);
+  expect(turn_index).toBeNull();
+  const username_content = $('#username').text();
+  expect(username).toMatch(username_content);
+  const room_id_content = $('#room_id').text();
+  expect(room_id).toMatch(room_id_content);
+  expect(checklist).toMatch(`<ul id=checklist class="list-group list-group-flush">
                       <li class="list-group-item">
                         <div class="custom-control custom-checkbox">
                           <input type="checkbox" class="custom-control-input" id=movement_checklist>
@@ -62,4 +68,14 @@ test('Test constants are as expected', () => {
                         </div>
                       </li>
                     </ul>`)
+  expect(namespace).toMatch('/combat');
+  // const io_namespace = io(namespace)
+  // expect(socket).toMatch(io_namespace);
+});
+
+test('Set Initiative', () => {
+  document.body.innerHTML = html;
+  const set_initiative = document.getElementById('set_initiative');
+  set_initiative.submit();
+  expect(document.getElementById('initiative_roll').value).toBe("");
 });
